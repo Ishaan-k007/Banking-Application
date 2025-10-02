d1f2c3c63e960317cdfa8f385b55b1bd8f69d325
Ishaan-k007 <khullarishaan21@gmail.com>
2025-10-02 21:24:48 +0100

    Adding better coding practises

--- app.py
+++ app.py
@@ -1,33 +1,34 @@
-from flask import Flask, render_template  # Add render_template
+from flask import Flask, render_template 
 from flask_login import LoginManager
 from config import Config
 from models import db
-from models.account import AccountTable
-from models.transaction import Transaction
 from models.user import User
-
 from routes.auth_routes import auth
 from routes.account_routes import account
 from routes.user_routes import user
 from routes.transaction_routes import transaction
-from services import account_service, auth_service, transaction_service
 
+# Initialize Flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
 # Initialize LoginManager
-login_manager = LoginManager() ## this is needed for flask login features eg. login_user 
+login_manager = LoginManager() 
 login_manager.init_app(app)
 
 @login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+def load_user(user_id: str):
+    """ Returns a userID for Flask-Login, or None if the user does not exist. """
+    try:
+        uid = int(user_id)
+    except (ValueError, TypeError):
+        return None
+    return User.query.get(uid)
 
 # Initialise a database 
 db.init_app(app)
-with app.app_context():
-    db.create_all()
+
 
 # Register blueprints
 app.register_blueprint(auth, url_prefix='/')
@@ -40,4 +41,6 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
